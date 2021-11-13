42f7b4915cb22fc2b40f2dece653ff59b0ec86a8
simran2337 <simranmittal2337@gmail.com>
2021-11-14 00:03:57 +0530

    cron updated

--- utils/db-clearer.js
+++ utils/db-clearer.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs');
 
 module.exports = () => {
-	cron.schedule(`0 */30 * * * *`, () => {
+	cron.schedule(`0 0 */12 * * *`, () => {
 		Video.find({ status: true }, (err, videos) => {
 			console.log({ err, count: videos.length });
 			videos.forEach(video => {
